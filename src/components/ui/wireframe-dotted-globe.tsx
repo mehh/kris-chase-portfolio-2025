23a87ee7223db7f43950cd4fae29216024615b2b
mehh <kris@mehh.org>
2025-11-05 12:16:05 -0600

    Fixing globe stuff

--- src/components/ui/wireframe-dotted-globe.tsx
+++ src/components/ui/wireframe-dotted-globe.tsx
@@ -254,7 +254,7 @@ export default function RotatingEarth({ width = 800, height = 600, className = "
     }
 
     // Set up rotation and interaction
-    const rotation = [0, 0]
+    const rotation: [number, number] = [0, 0]
     let autoRotate = true
     const rotationSpeed = 0.5
 
@@ -273,7 +273,7 @@ export default function RotatingEarth({ width = 800, height = 600, className = "
       autoRotate = false
       const startX = event.clientX
       const startY = event.clientY
-      const startRotation = [...rotation]
+      const startRotation: [number, number] = [rotation[0], rotation[1]]
 
       const handleMouseMove = (moveEvent: MouseEvent) => {
         const sensitivity = 0.5
